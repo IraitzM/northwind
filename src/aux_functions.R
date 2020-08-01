e1f5c8e66c414e9dc368782fb80b404e5c6e30ce
IraitzM <iraitzm@gmail.com>
2020-08-01 18:26:12 +0200

    Data model created

--- src/aux_functions.R
+++ src/aux_functions.R
@@ -1,39 +1,37 @@
-# Librerias
+# Required libraries
 require(DBI)
 require(dplyr)
 require(arsenal)
+require(DiagrammeR)
+devtools::install_github("bergant/datamodelr")
 
 # Following function erases the table if exists
 # Parameters:
-# -> conection : database conection
+# -> connection : database connection
 # -> name: table name
 
 eraseIfExists <- function(con, name)
 {
   if(dbExistsTable(con, name)){
-    dbRemoveTable(con, name)
+    dbRemoveTable(con, name, cascade=TRUE)
   }
 }
 
 # Clear database
 # Parameters:
-# -> conection : database conection
+# -> connection : database connection
 clearDB <- function(con){
-  tList <- list(con)
-  for (l in tList){
-    eraseIfExists(con, l)
-  }
+  exec(con, "DROP TABLE employees, products, orders, order_details, customers CASCADE;")
 }
 
 # List existing tables
 # Parameters:
-# -> conection : database conection
-
+# -> connection : database connection
 list <- dbListTables
 
 # Store table
 # Parameters:
-# -> conection : database conection
+# -> connection : database connection
 # -> dataframe : table data to be stored
 # -> name : table name
 storeTable <- function(con,dataframe, name){
@@ -43,8 +41,38 @@ storeTable <- function(con,dataframe, name){
 
 # Send query
 # Parameters:
-# -> conection : database conection
+# -> connection : database connection
 # -> query : query to be sent
 query <- function(con, query){
   dbFetch(dbSendQuery(con, query))
+}
+
+# Exec command
+# Parameters:
+# -> connection : database connection
+# -> query : query to be sent
+exec <- function(con, query){
+  res<-dbSendStatement(con, query)
+  dbClearResult(res) 
+}
+
+# Reads data and writes it to a given table
+# Parameters:
+# -> connection : database connection
+# -> fname : file name matching table name
+load <- function(con, fname){
+  df <- read.csv(paste0("data/", fname, ".csv"), as.is=T)
+  colnames(df)<-tolower(colnames(df))
+  dbAppendTable(con, fname, df)
+}
+
+# Show model
+# Parameters:
+# -> connection : database connection
+show <- function(con){
+  sQuery <- datamodelr::dm_re_query("postgres")
+  dm <- dbGetQuery(con, sQuery) 
+  dm <- datamodelr::as.data_model(dm)
+  graph <- datamodelr::dm_create_graph(dm, rankdir = "RL")
+  datamodelr::dm_render_graph(graph)
 }
--- src/building_the_model.R
+++ src/building_the_model.R
@@ -1,29 +1,174 @@
 # Let's explore how to model our retail database
-# First we will import required libraries to interact with our local PostgreSQL RDBMS
-
-# Software
-# PostgreSQL 10.2.5
-
-# Libraries
+# First we will need to import required libraries to interact with our local PostgreSQL RDBMS
 renv::restore()
-#
-devtools::install_github("bergant/datamodelr")
 
 # Auxiliary functions
 source('src/aux_functions.R')
 
+# Software
+# PostgreSQL
+
+# Could be used Docker for this:
+# 1. docker pull postgres
+# 2. docker run --name some-postgres -e POSTGRES_PASSWORD=postgres -d postgres
+# 3. docker inspect some-postgres
+dhost <- '172.17.0.2'
+
 # Let's connect to our database
-con <- DBI::dbConnect(RPostgres::Postgres(), user= 'postgres', password = 'postgres')
+con <- DBI::dbConnect(RPostgres::Postgres(), 
+                      user= 'postgres', 
+                      password = 'postgres', 
+                      host=dhost)
 
 # List existing tables
 list(con)
 
-# Create relational model
+# Create relations #############################################################
+
+# Customers
+q <- 'CREATE TABLE customers (
+    customerid bpchar NOT NULL,
+    companyname character varying(40) NOT NULL,
+    contactname character varying(30),
+    contacttitle character varying(30),
+    address character varying(60),
+    city character varying(15),
+    region character varying(15),
+    postalcode character varying(10),
+    country character varying(15),
+    phone character varying(24),
+    fax character varying(24)
+);'
+exec(con,q)
+
+# Employees
+q <- 'CREATE TABLE employees (
+    employeeid smallint NOT NULL,
+    lastnname character varying(20) NOT NULL,
+    firstname character varying(10) NOT NULL,
+    title character varying(30),
+    titleofcourtesy character varying(25),
+    birthdate date,
+    hiredate date,
+    address character varying(60),
+    city character varying(15),
+    region character varying(15),
+    postalcode character varying(10),
+    country character varying(15),
+    homephone character varying(24),
+    extension character varying(4),
+    photo bytea,
+    notes text,
+    reportsto smallint,
+    photopath character varying(255)
+);'
+exec(con, q)
+
+# Order details
+q <- 'CREATE TABLE order_details (
+    orderid smallint NOT NULL,
+    productid smallint NOT NULL,
+    unitprice real NOT NULL,
+    quantity smallint NOT NULL,
+    discount real NOT NULL
+);'
+exec(con, q)
+
+# Orders
+q <- 'CREATE TABLE orders (
+    orderid smallint NOT NULL,
+    customerid bpchar,
+    employeeid smallint,
+    orderdate date,
+    requireddate date,
+    shippeddate date,
+    shipvia smallint,
+    freight real,
+    shipname character varying(40),
+    shipaddress character varying(60),
+    shipcity character varying(15),
+    shipregion character varying(15),
+    shippostalcode character varying(10),
+    shipcountry character varying(15)
+);'
+exec(con, q)
+
+# Products
+q <- 'CREATE TABLE products (
+    productid smallint NOT NULL,
+    productname character varying(40) NOT NULL,
+    supplierid smallint,
+    categoryid smallint,
+    quantityper_unit character varying(20),
+    unitprice real,
+    unitsinstock smallint,
+    unitsonorder smallint,
+    reorderlevel smallint,
+    discontinued integer NOT NULL
+);'
+exec(con, q)
+
+# List existing tables
+list(con)
+
+# Show the tables
+show(con)
+
+# Constraints ##################################################################
+# Add PK to customers
+q <-'ALTER TABLE ONLY customers
+ADD CONSTRAINT pk_customers PRIMARY KEY (customerid);'
+exec(con, q)
+  
+q <-'ALTER TABLE ONLY employees
+ADD CONSTRAINT pk_employees PRIMARY KEY (employeeid);'
+exec(con, q)
+
+q <-'ALTER TABLE ONLY order_details
+ADD CONSTRAINT pk_order_details PRIMARY KEY (orderid, productid);'
+exec(con, q)
+  
+q <-'ALTER TABLE ONLY orders
+ADD CONSTRAINT pk_orders PRIMARY KEY (orderid);'
+exec(con, q)
+  
+q <-'ALTER TABLE ONLY products
+ADD CONSTRAINT pk_products PRIMARY KEY (productid);'
+exec(con, q)
+
+q <-'ALTER TABLE ONLY orders
+ADD CONSTRAINT fk_orders_customers FOREIGN KEY (customerid) REFERENCES customers;'
+exec(con, q)
+
+q <-'ALTER TABLE ONLY orders
+ADD CONSTRAINT fk_orders_employees FOREIGN KEY (employeeid) REFERENCES employees;'
+exec(con, q)
+
+q <-'ALTER TABLE ONLY order_details
+ADD CONSTRAINT fk_order_details_products FOREIGN KEY (productid) REFERENCES products;'
+exec(con, q)
+
+q <-'ALTER TABLE ONLY order_details
+ADD CONSTRAINT fk_order_details_orders FOREIGN KEY (orderid) REFERENCES orders;'
+exec(con, q)
+
+q <-'ALTER TABLE ONLY employees
+ADD CONSTRAINT fk_employees_employees FOREIGN KEY (reportsto) REFERENCES employees;'
+exec(con, q)
 
 # Show it
+show(con)
 
+# Get data in ##################################################################
 
-# Eliminamos las tablas
+# Orders
+load(con, "orders")
+load(con, "products")
+load(con, "order_details")
+load(con, "customers")
+load(con, "employees")
+
+# Clear the database
 clearDB(con)
 
 # Disconnect from the database
